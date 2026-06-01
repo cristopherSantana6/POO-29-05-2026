80cec6957c0958dd0b714e489ac0031359fa7fa6
cristopherSantana6 <cristopherrafael644@gmail.com>
2026-06-01 15:41:30 -0600

    cuarta practica

---
+++ 4/facturacion/src/main/java/com/tuempresa/facturacion/modelo/Factura.java
@@ -0,0 +1,58 @@
+package com.tuempresa.facturacion.modelo;
+
+import java.time.*;
+import java.util.Collection;
+import javax.persistence.*;
+
+import com.tuempresa.facturacion.calculadores.CalculadorSiguienteNumeroParaAnyo;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.*;
+import org.openxava.calculators.*;
+import lombok.*;
+@View(members= // Esta vista no tiene nombre, por tanto será la vista usada por defecto
+        "anyo, numero, fecha;" + // Separados por coma significa en la misma línea
+                "cliente;" + // Punto y coma significa nueva línea
+                "detalles;" +
+                "observaciones"
+
+)
+@ManyToOne(fetch=FetchType.LAZY, optional=false)
+@ReferenceView("Simple") // La vista llamada 'Simple' se usará para visualizar esta referencia
+@Entity @Getter @Setter
+public class Factura {
+
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @Hidden
+    @GenericGenerator(name="system-uuid", strategy="uuid")
+    @Column(length=32)
+    String oid;
+
+    @Column(length=4)
+    @DefaultValueCalculator(CurrentYearCalculator.class) // Año actual
+    int anyo;
+
+    @Column(length=6)
+    @DefaultValueCalculator(value= CalculadorSiguienteNumeroParaAnyo.class,
+            properties=@PropertyValue(name="anyo") // Para inyectar el valor de anyo de Factura
+            // en el calculador antes de llamar a calculate()
+    )
+    int numero;
+
+    @ManyToOne(fetch=FetchType.LAZY, optional=false) // El cliente es obligatorio
+    Cliente cliente;
+
+
+    @Required
+    @DefaultValueCalculator(CurrentLocalDateCalculator.class) // Fecha actual
+    LocalDate fecha;
+
+    @TextArea
+    String observaciones;
+
+    @ElementCollection
+    Collection<Detalle> detalles;
+
+
+
+}
